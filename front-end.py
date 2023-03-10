e6da2fd5586833ed287f72aadaaf102b259ac1c7
Umar Khan <dragorific@users.noreply.github.com>
2023-03-10 14:32:04 -0500

    working commit for bronze milestone
    updated openai engine, exported python functions to be used in conjunction with one-another, and built cursory python-android app, loaded assets

---
+++ front-end.py
@@ -0,0 +1,150 @@
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.image import Image
+from kivy.uix.gridlayout import GridLayout
+from kivy.graphics import Rectangle
+from kivy.uix.popup import Popup
+from kivy.uix.textinput import TextInput
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.spinner import Spinner
+import subprocess
+import json
+
+class SettingsForm(Popup):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.title = "Settings"
+        self.size_hint = (0.6, 0.6)
+
+        self.form_layout = GridLayout(cols=2)
+
+        # Add the "Age" field
+        self.form_layout.add_widget(Label(text="Age"))
+        self.age_input = Spinner(
+            text='Select Age',
+            values=["{:02d}".format(x) for x in range(16,81)],
+            size_hint=(None, None),
+            size=(200, 50)
+        )
+        self.form_layout.add_widget(self.age_input)
+
+        # Add the "Weight" field
+        self.form_layout.add_widget(Label(text="Weight"))
+        self.weight_input = Spinner(
+            text='Select Weight in Pounds(lbs)',
+            values=["{:02d}".format(x) for x in range(80, 300, 2)],
+            size_hint=(None, None),
+            size=(200, 50)
+        )
+        self.form_layout.add_widget(self.weight_input)
+
+        # Add the "Gender" field
+        self.form_layout.add_widget(Label(text="Gender"))
+        self.gender_input = Spinner(
+            text='Select Gender',
+            values=['Male', 'Female'],
+            size_hint=(None, None),
+            size=(200, 50)
+        )
+        self.form_layout.add_widget(self.gender_input)
+
+        # Add the "Height" field
+        self.form_layout.add_widget(Label(text="Height"))
+        self.height_input = Spinner(
+            text='Select Height in cm',
+            values=['150', '160', '170', '180', '190', '200'],
+            size_hint=(None, None),
+            size=(200, 50)
+        )
+        self.form_layout.add_widget(self.height_input)
+
+        # Add the "Submit" button
+        self.submit_button = Button(text="Submit", font_size=20)
+        self.submit_button.bind(on_press=self.save_form_data)
+
+        # Add a BoxLayout to center the "Submit" button
+        button_box = BoxLayout(orientation='horizontal', size_hint_y=None, height=50)
+        button_box.add_widget(Label())
+        button_box.add_widget(self.submit_button)
+        button_box.add_widget(Label())
+        self.form_layout.add_widget(button_box)
+
+        self.add_widget(self.form_layout)
+
+    def save_form_data(self, instance):
+        data = {
+            "user-id": "test_id_123",
+            "user-info": 
+            {
+                "age": self.age_input.text,
+                "weight": self.weight_input.text,
+                "gender": self.gender_input.text,
+                "height": self.height_input.text,
+            }
+        }
+
+        # Save the data to a text file in JSON format
+        with open("data.txt", "a") as f:
+            f.write(json.dumps(data))
+            f.write("\n")
+            f.close()
+
+        # Close the popup
+        self.dismiss()
+
+    
+
+class MyGridLayout(GridLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.cols = 1
+
+        # Add the logo
+        self.logo = Image(source="assets/logo.png")
+        self.add_widget(self.logo)
+
+        # Add the title
+        self.title = Label(text="DePhlate AI Diet Tracker", font_size=40)
+        self.add_widget(self.title)
+
+        # Load the background image using a rectangle
+        with self.canvas.before:
+            self.background = Image(source="assets/dephlate_bg2.jpg").texture
+            self.rect = Rectangle(size=self.size, pos=self.pos, texture=self.background)
+            self.bind(size=self._update_rect, pos=self._update_rect)
+
+                # Add the "Start" button
+        self.start_button = Button(text="Start", font_size=40, size_hint=(None, 1), width=300, size_hint_x=0.4)
+        self.start_button.bind(on_press=self.open_program)
+
+        # Add the "Settings" button
+        self.settings_button = Button(text="Settings", font_size=40, size_hint=(None, 1), width=300, size_hint_x=0.4)
+        self.settings_button.bind(on_press=self.open_settings)
+
+        # Create a BoxLayout to center the two buttons horizontally
+        button_box = BoxLayout(orientation='horizontal', size_hint_y=0.3, width=self.width, spacing=20)
+        button_box.padding = (50, 0, 50, 20)             # Set the left and right padding to 50 pixels, (left, top, right, bottom)
+        button_box.add_widget(self.start_button)
+        button_box.add_widget(self.settings_button)
+        button_box.align = 'center'                     # Set the align property to center the buttons horizontally
+        self.add_widget(button_box)
+
+    def open_program(self, instance):
+        subprocess.Popen(['python', 'camera_stream.py'])
+
+    def open_settings(self, instance):
+        form = SettingsForm()
+        form.open()
+
+    def _update_rect(self, instance, value):
+        self.rect.pos = instance.pos
+        self.rect.size = instance.size
+
+class MyKivyApp(App):
+    def build(self):
+        return MyGridLayout()
+
+if __name__ == "__main__":
+    MyKivyApp().run()
